ef449206a75ea890d584b93a818eb8787dda98c9
Lauritz Klünder <lauritz.kluender@yahoo.de>
2017-11-27 16:48:22 +0100

    lala

--- V353/Diagramm2.py
+++ V353/Diagramm2.py
@@ -5,12 +5,13 @@ from scipy.stats import sem
 
 y, x = np.genfromtxt('Tabelle2.txt', unpack=True)
 
-def f(x, a, b, c):
-    y = a*np.arctan(b*x) + c
+def f(x, a):
+    y = 1 / np.sqrt(1 + (a*x)**2)
     return y
 
-x_plot = np.linspace(0, 10e3)
-params, covariance_matrix = curve_fit(f, x, y, p0=(-1, 5, 1))
+x_plot = np.linspace(0, 10e3, 1000)
+
+params, covariance_matrix = curve_fit(f, x, y)
 
 errors = np.sqrt(np.diag(covariance_matrix))
 
@@ -19,12 +20,12 @@ plt.plot(x_plot, f(x_plot, *params), 'r-', label='Regression')
 #plt.xlabel('t / sms')
 plt.xscale('log')
 #plt.xlim(0, 10e3)
-plt.ylim(0, 0.5)
+#plt.ylim(0, 0.5)
 #plt.tight_layout(pad=0)
 plt.savefig('Diagramm2.pdf')
 
 print('a=', params[0],'+-', errors[0])
-print('b=', params[1],'+-', errors[1])
+
 #default_seed = (1,1,1)
 #good_seed = (-1,1,1)
 #parameter = [default_seed, good_seed, params]
