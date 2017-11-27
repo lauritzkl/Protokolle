f7483a7dce04d6ec2e385b1165214abca9bc75fb
Lauritz Klünder <lauritz.kluender@yahoo.de>
2017-11-27 15:46:18 +0100

    plot

--- V353/Diagramm2.py
+++ V353/Diagramm2.py
@@ -5,22 +5,24 @@ from scipy.stats import sem
 
 y, x = np.genfromtxt('Tabelle2.txt', unpack=True)
 
-def sigmoid(x, a, b,):
-    y = a / (1 + np.exp(-(x-b))) 
+def f(x, a, b, c):
+    y = a*np.arctan(b*x) + c
     return y
 
-params, covariance_matrix = curve_fit(sigmoid, x, y, p0=(1,1))
+x_plot = np.linspace(0, 10e3)
+params, covariance_matrix = curve_fit(f, x, y, p0=(-1, 5, 1))
 
 errors = np.sqrt(np.diag(covariance_matrix))
 
 plt.plot(x, y, 'kx',)
-plt.plot(x, sigmoid(x, *params), 'r-', label='Regression')
+plt.plot(x_plot, f(x_plot, *params), 'r-', label='Regression')
 #plt.xlabel('t / sms')
 plt.xscale('log')
-plt.xlim(0, 10e3)
+#plt.xlim(0, 10e3)
 plt.ylim(0, 0.5)
-plt.tight_layout(pad=0)
+#plt.tight_layout(pad=0)
 plt.savefig('Diagramm2.pdf')
+
 print('a=', params[0],'+-', errors[0])
 print('b=', params[1],'+-', errors[1])
 #default_seed = (1,1,1)
