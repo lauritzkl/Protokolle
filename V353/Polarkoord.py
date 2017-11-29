a2956654f700a4f850bb01d7ce0d0a649ceef474
Robert Konradi <robert.konradi@gmx.net>
2017-11-29 20:01:19 +0100

    Sehr neu

--- V353/Polarkoord.py
+++ V353/Polarkoord.py
@@ -11,11 +11,8 @@ def f(x, a, b):
 params, covariance_matrix = curve_fit(f, x, y)
 
 x_plot = np.linspace(0, 1.6, 1000)
-
 plt.polar(x, y, 'kx', label='Messwerte')
 plt.polar(x_plot, f(x_plot, *params), 'r-', label='Regression')
 #plt.plot(x_plot)
 plt.legend()
-
-
 plt.savefig('Polar.pdf')
