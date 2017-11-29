69967fba342d3aaf7f09c83960a46f4530fdeebb
Lauritz Klünder <lauritz.kluender@yahoo.de>
2017-11-29 17:18:54 +0100

    Polarplot

--- V353/Polarkoord.py
+++ V353/Polarkoord.py
@@ -2,11 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-x, y = np.genfromtxt('Tabelle4.txt', unpack=True)
+x, y = np.genfromtxt('Tabelle4_1.txt', unpack=True)
 
-x_plot= np.linspace(0, 50, 100)
+def f(x, a, b):
+    y = -(x/a) + b
+    return y
 
-plt.polar(x, y)
+params, covariance_matrix = curve_fit(f, x, y)
+
+x_plot = np.linspace(0, 1.6, 1000)
+
+plt.polar(x, y, 'kx', label='Messwerte')
+plt.polar(x_plot, f(x_plot, *params), 'r-', label='Regression')
 #plt.plot(x_plot)
 plt.legend()
 
