29a2e135ae70672cd3280fb845de790bbd03cb45
Robert Konradi <robert.konradi@gmx.net>
2017-12-27 22:18:24 +0100

    lala

--- V303/plot2.py
+++ V303/plot2.py
@@ -18,13 +18,14 @@ def f(x, a, b):
     return y
 
 x_plot = np.linspace(0, 5, 1000)
-
+t = np.linspace(0, 5, 1000)
 params, covariance_matrix = curve_fit(f, x, y)
 
 errors = np.sqrt(np.diag(covariance_matrix))
 #b ist die offzielle Phasenverschiebung aber ich habe eine Value error
 plt.plot(x, y, r'kx', label='Messwerte')
 plt.plot(x_plot, f(x_plot, *params), 'r-', label='Regression')
+plt.plot(t, f(t, *params), 'b-', label='Theoriekurve')
 plt.xticks([0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi, 5*np.pi/4, 3*np.pi/2],
             [r"$0$", r"$\frac{\pi}{4}$", r"$\frac{\pi}{2}$",  r"$\frac{3\pi}{4}$", r"$\pi$",
              r"$\frac{5\pi}{4}$", r"$\frac{3\pi}{2}$"])
---
+++ V303/plot3.py
@@ -0,0 +1,29 @@
+from scipy.optimize import curve_fit
+import numpy as np
+import matplotlib.pyplot as plt
+# from uncertainties import ufloat
+U, p = np.genfromtxt('data2.txt', unpack=True)
+phi = np.linspace(0, 5, 1000)
+t = np.linspace(0, 5, 1000)
+#-0.3, 8, 1000
+def f(x, a, b, c):
+    return b * np.cos(x - a) + c
+
+params, cov = curve_fit(f, p, U)
+errP = np.sqrt(np.diag(cov))
+print('a =', params[0], '+/-', errP[0])
+print('b =', params[1], '+/-', errP[1])
+print('c =', params[2], '+/-', errP[2])
+plt.plot(p, U, 'kx', label='Messwerte')
+plt.plot(phi, f(phi, *params), 'r-', label='Ausgleichskurve')
+plt.plot(t, f(t, 0, params[1], 0), 'g-', label='Theoriekurve')
+plt.xlabel(r'$\varphi\:/\:rad$')
+plt.ylabel(r'$U\:/\:V$')
+plt.legend(loc='best')
+plt.xticks([0, np.pi / 4, np.pi / 2, 3 * np.pi / 4, np.pi, 5 * np.pi / 4, 3 * np.pi / 2, 7 * np.pi / 4, 2 * np.pi],
+           [r"$0$", r"$\frac{1}{4}\pi$", r"$\frac{1}{2}\pi$", r"$\frac{3}{4}\pi$", r"$\pi$", r"$\frac{5}{4}\pi$", r"$\frac{3}{2}\pi$", r"$\frac{7}{4}\pi$", r"$2\pi$"])
+plt.xlim(-0.2, 0.2 + 2 * np.pi)
+plt.grid()
+plt.tight_layout()
+plt.savefig('kurve1.pdf')
+plt.show()
